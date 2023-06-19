210710a0e774c9106c24901439d63a6d41a24b2d
javierandrango <lab_iarobotico593@outlook.com>
2023-06-19 12:07:53 -0500

    add html form inside python

--- vagrant/newsdata/newsreport.py
+++ vagrant/newsdata/newsreport.py
@@ -4,6 +4,8 @@
 
 import psycopg2
 import numpy as np
+import pandas as pd
+from datetime import date
 
 DBNAME = "newsdata"
 
@@ -60,8 +62,9 @@ queries.append(
     GROUP BY total_status.days,total_status.t_status,error_status.e_status;
     '''
 )
-#print individual report in txt file
+
 '''
+# print individual report in txt file
 for count in range(0, len(queries)):
     c.execute(queries[count])
     report = c.fetchall()
@@ -69,9 +72,76 @@ for count in range(0, len(queries)):
 '''
 
 reportList = []
-for count in range(0, len(queries)):
-    c.execute(queries[count])
-    reportList.append(np.array(c.fetchall(),dtype='str'))
-    
+for query in queries:
+    c.execute(query)
+    reportList.append(np.array(c.fetchall(), dtype='str'))
+
 db.close()
 
+pd.set_option('display.colheader_justify', 'left')
+# What are the most popular three articles of all time?
+dfReport1 = pd.DataFrame(reportList[0], columns=['Article name', 'Total Views'])
+
+# Who are the most popular article authors of all time?
+dfReport2 = pd.DataFrame(reportList[1], columns=['Author', 'Total Views'])
+
+# On which days did more than 1% of requests lead to errors?
+data = [[reportList[2][0][0][:11], str(float(reportList[2][0][2])/float(reportList[2][0][1])*100)]]
+dfReport3 = pd.DataFrame(data, columns=['Year', 'Percentage'])
+
+pageTitle = 'Report'
+
+# Textual month, day and year
+day = date.today()
+datenow = day.strftime("%B %d, %Y")
+
+report1Title = 'What are the most popular three articles of all time?'
+report2Title = 'Who are the most popular article authors of all time?'
+report3Title = 'On which days did more than 1% of requests lead to errors?'
+
+html = '''
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Python DB-API report</title>
+    <style media="screen">
+      p{
+        color: blue;
+        text-align: center;
+      }
+      h1{
+        color: green;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>%s</h1>
+    <p>Report log analysis %s</p>
+    <!--report to question 1-->
+    <div>
+        <h2>%s</h2>
+        <p>%s</p>
+    </div>
+    <!--report to question 2-->
+    <div>
+        <h2>%s</h2>
+        <p>%s</p>
+    </div>
+    <!--report to question 3-->
+    <div>
+        <h2>%s</h2>
+        <p>%s</p>
+    </div>
+
+  </body>
+</html>
+'''%(pageTitle,datenow,
+     report1Title,dfReport1.to_html(),
+     report2Title,dfReport2.to_html(),
+     report3Title,dfReport3.to_html())
+
+
+with open('report_db_api.html','w') as f:
+    f.write(html)
