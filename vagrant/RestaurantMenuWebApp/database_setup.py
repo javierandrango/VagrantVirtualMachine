4a628dfc1bbdf19f1583b5ec97fe9a4422dd8f29
javierandrango <lab_iarobotico593@outlook.com>
2023-07-04 14:21:43 -0500

    create DB

--- vagrant/RestaurantMenuWebApp/database_setup.py
+++ vagrant/RestaurantMenuWebApp/database_setup.py
@@ -5,7 +5,23 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class Restaurant(Base):
-    pass
+    __tablename__ = 'restaurant'
+    name = Colum(String(250), nullable=False)
+    id = Colum(Integer, primary_key=True)
+
+
 class MenuItem(Base):
-    pass
+    __tablename__ = 'menuitem'
+    name = Colum(String(250), nullable=False)
+    id = Colum(Integer, primary_key=True)
+    course = Colum(String(250))
+    description = Colum(String(250))
+    price = Colum(String(250))
+    restaurant_id = Colum(Integer, nullable=False)
+    restaurant = relationship(Restaurant)
+
+
+engine = create_engine('sqlite:///restaurant.db')
+Base.metadata.create_all(engine)
