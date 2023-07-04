7d7a97218e941fa673d5af29c20b51f5aa411a18
javierandrango <lab_iarobotico593@outlook.com>
2023-07-04 18:19:36 -0500

    render html templates

--- vagrant/RestaurantMenuWebApp/restaurants.py
+++ vagrant/RestaurantMenuWebApp/restaurants.py
@@ -6,52 +6,60 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/restaurants/')
 def restaurants():
-    return "home page"
+    return render_template('restaurants.html')
 
 
 # add a new restaurant
 @app.route('/restaurants/new/')
-def newRestaurants():
-    return "add new restaurant"
+def newRestaurant():
+    return render_template('newRestaurant.html')
 
 
 # edit a restaurant
 @app.route('/restaurants/<int:restaurant_id>/edit/')
 def editRestaurant(restaurant_id):
-    return "edit a restaurant"
+    return render_template('editRestaurant.html',
+                           restaurant_id=restaurant_id)
 
 
 # delete a restaurant
 @app.route('/restaurants/<int:restaurant_id>/delete/')
 def deleteRestaurant(restaurant_id):
-    return "delete a restaurant"
+    return render_template('deleteRestaurant.html',
+                           restaurant_id=restaurant_id)
 
 
 # restaurant menu principal (all menu items)
 @app.route('/restaurants/<int:restaurant_id>/')
 @app.route('/restaurants/<int:restaurant_id>/menu/')
 def menu(restaurant_id):
-    return "restaurant menu"
+    return render_template('menu.html',
+                           restaurant_id=restaurant_id)
 
 
 # add a new menu restaurant
 @app.route('/restaurants/<int:restaurant_id>/menu/new')
 def newMenu(restaurant_id):
-    return "add a new menu restaurant"
+    return render_template('newMenu.html',
+                           restaurant_id=restaurant_id)
 
 
-# edit a menu item 
+# edit a menu item
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menuitem_id>/edit')
-def editMenuItem(restaurant_id,menuitem_id):
-    return "edit a specific menu item"
+def editMenuItem(restaurant_id, menuitem_id):
+    return render_template('editMenuItem.html',
+                           restaurant_id=restaurant_id,
+                           menuitem_id=menuitem_id)
 
 
 # delete a menu item
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menuitem_id>/delete')
-def deleteMenuItem(restaurant_id,menuitem_id):
-    return "delete a specific menu item"
+def deleteMenuItem(restaurant_id, menuitem_id):
+    return render_template('deleteMenuItem.html',
+                           restaurant_id=restaurant_id,
+                           menuitem_id=menuitem_id)
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
