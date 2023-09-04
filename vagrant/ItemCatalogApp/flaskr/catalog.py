cdbefea5baef3d13535e2d0e9a942f66499448ce
javierandrango <lab_iarobotico593@outlook.com>
2023-09-04 16:48:52 -0500

    create a simple flask app

--- vagrant/ItemCatalogApp/flaskr/catalog.py
+++ vagrant/ItemCatalogApp/flaskr/catalog.py
@@ -1,10 +1,14 @@
+# flask web framework
 from flask import Flask
 
+# create flask app
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "hello"
+
+# app routes
+@app.route('/')
+def main_page():
+    return "main_page"
 
 if __name__ == "__main__":
     app.debug = True
